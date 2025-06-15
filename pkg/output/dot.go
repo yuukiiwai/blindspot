97ffcf8fb09416865426bdb1d4e501e9c165201c
yuukiiwai <83199691+yuukiiwai@users.noreply.github.com>
2025-06-15 21:05:31 +0900

    del not sure implement

--- pkg/output/dot.go
+++ pkg/output/dot.go
@@ -1,8 +1,7 @@
 package output
 
 import (
-	"fmt"
-	"strings"
+	"errors"
 
 	"github.com/yuukiiwai/blindspot/pkg/core"
 )
@@ -17,45 +16,6 @@ func NewDotFormatter() *DotFormatter {
 
 // Format ステートマシンをDOT形式で出力
 func (f *DotFormatter) Format(generator *core.Generator) (string, error) {
-	var dot strings.Builder
-	dot.WriteString("digraph G {\n")
-	dot.WriteString("    rankdir=LR;\n")
-	dot.WriteString("    node [shape=box];\n\n")
-
-	// ノードの出力
-	for _, node := range generator.GetNodes() {
-		nodeID := getDotNodeID(node)
-		label := getDotNodeLabel(node)
-		dot.WriteString(fmt.Sprintf("    %s [label=\"%s\"];\n", nodeID, label))
-	}
-
-	dot.WriteString("\n")
-
-	// エッジの出力
-	for _, edge := range generator.GetEdges() {
-		fromID := getDotNodeID(edge.GetFrom())
-		toID := getDotNodeID(edge.GetTo())
-		edgeLabel := edge.GetRule().GetName()
-		dot.WriteString(fmt.Sprintf("    %s -> %s [label=\"%s\"];\n", fromID, toID, edgeLabel))
-	}
-
-	dot.WriteString("}\n")
-	return dot.String(), nil
-}
-
-// getDotNodeID ノードのDOT IDを生成
-func getDotNodeID(node *core.Node) string {
-	id := node.GetID()
-	if id == "empty" {
-		return "empty"
-	}
-	return strings.ReplaceAll(strings.ReplaceAll(id, ",", "_"), " ", "_")
-}
-
-// getDotNodeLabel ノードのDOT表示名を生成
-func getDotNodeLabel(node *core.Node) string {
-	if len(node.GetResources()) == 0 {
-		return "empty"
-	}
-	return strings.Join(node.GetResources(), "\\n")
+	// TODO: Implement
+	return "", errors.New("not implemented")
 }
